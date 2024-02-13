1b5cab3739a6f00bc50595cba2d753c5b4c5bfec
Alberto-Arrighetti <alberto.arrighetti1571@gmail.com>
2024-02-13 14:41:57 +0100

    array con le informazioni

---
+++ js/script.js
@@ -0,0 +1,38 @@
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// Creare l’array di oggetti con le informazioni fornite.
+const membersInfo = [
+    {
+        fullName: "Wayne Barnett",
+        role: "Founder & CEO",
+        picture: "wayne-barnett-founder-ceo.jpg",
+    },
+    {
+        fullName: "Angela Caroll",
+        role: "Chief Editor",
+        picture: "angela-caroll-chief-editor.jpg",
+    },
+    {
+        fullName: "Walter Gordon",
+        role: "Office Manager",
+        picture: "walter-gordon-office-manager.jpg",
+    },
+    {
+        fullName: "Angela Lopez",
+        role: "Social Media Manager",
+        picture: "angela-lopez-social-media-manager.jpg",
+    },
+    {
+        fullName: "Scott Estrada",
+        role: "Developer",
+        picture: "scott-estrada-developer.jpg",
+    },
+    {
+        fullName: "Barbara Ramos",
+        role: "Graphic Designer",
+        picture: "barbara-ramos-graphic-designer.jpg",
+    },
+]
+
+console.log(membersInfo);
